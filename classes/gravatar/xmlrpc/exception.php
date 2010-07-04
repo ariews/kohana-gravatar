809e603e48a55cd6f1b9fc3b5e6b50a827cb106f
Arie W. Subagja <arie@wr0x.(none)>
2010-07-05 04:17:42 +0700

    easy to extends

--- classes/gravatar/xmlrpc/exception.php
+++ classes/gravatar/xmlrpc/exception.php
@@ -1,3 +1,3 @@
 <?php defined('SYSPATH') or die('No direct script access.');
 
-class Gravatar_Xmlrpc_Exception extends Gravatar_Exception {}
+class Gravatar_Xmlrpc_Exception extends Kohana_Gravatar_Exception {}
--- classes/kohana/gravatar/exception.php
+++ classes/kohana/gravatar/exception.php
@@ -1,3 +1,3 @@
 <?php defined('SYSPATH') or die('No direct script access.');
 
-class Gravatar_Exception extends Kohana_Gravatar_Exception {}
+class Kohana_Gravatar_Exception extends Kohana_Exception { }
